0776b8e89e0ba618c44a3d57dc4dbfa01df2a5cd
dmllnnkk <133574712+dmllnnkk@users.noreply.github.com>
2023-08-30 13:20:27 +0300

    done

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const button = document. body.querySelector( '#button');
+$('.spoiler').hide();
 
-const butoonBox = document.body.querySelector ('#button-box');
-
-const handleClick = function (e) {
-   console. log('document.body.classList', document.body.classList) 
-   document. body.classList
-}
-
-button.addEventListener('click', handleClick)
-
-
-const input = document.body.querySelector('#input');
-
-const handleInput = function (e) {
-console. log (e.target .value);
-document.body. append (e.target.value)
-}
-input.addEventListener ('change', handleInput) ;
-
-alert()
+$(`.spoilerButton`).on('click', function () {
+  $(`.spoilerButton`).hide();
+  $('.spoiler').slideDown(2000);  
+});
