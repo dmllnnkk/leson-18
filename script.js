e73b59266d9cbdbdd3a6e7c81275b3c6aa1fb1f9
dmllnnkk <133574712+dmllnnkk@users.noreply.github.com>
2023-08-30 11:12:46 +0300

    done

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const button = document. body. querySelector( '#button');
+const button = document. body.querySelector( '#button');
 
-const butoonBox = document. body.querySelector ('#button-box');
+const butoonBox = document.body.querySelector ('#button-box');
 
-const handleClick = function () {
-   console. log(' document. body. classList', document.body.lassList) 
+const handleClick = function (e) {
+   console. log('document.body.classList', document.body.classList) 
    document. body.classList
 }
 
 button.addEventListener('click', handleClick)
 
-alert(handleClick)
 
-const input = document. body.querySelector ('#input');
+const input = document.body.querySelector('#input');
 
 const handleInput = function (e) {
 console. log (e.target .value);
-document.body. append (e. target. value)
+document.body. append (e.target.value)
 }
-input. addEventListener ('change', handleInput) ;
+input.addEventListener ('change', handleInput) ;
+
+alert()
