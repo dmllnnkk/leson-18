e3f5296d30e7140d60af3337f28d70002cb714af
dmllnnkk <133574712+dmllnnkk@users.noreply.github.com>
2023-09-21 16:08:11 +0300

    fixed

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+$("#myElement").css("color", "red");
+
+document.querySelector("#myElement").style.color = "red";
+
 $('.spoiler').hide();
 
-$(`.spoilerButton`).on('click', function () {
-  $(`.spoilerButton`).hide();
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
   $('.spoiler').slideDown(2000);  
 });
+
+
