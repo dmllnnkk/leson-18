e3812f6047121cc2d898775654f1168568278d88
dmllnnkk <133574712+dmllnnkk@users.noreply.github.com>
2023-10-04 11:58:06 +0300

    done

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-$("#myElement").css("color", "red");
-
-document.querySelector("#myElement").style.color = "red";
-
 $('.spoiler').hide();
 
 $('.spoilerButton').on('click', function () {
   $('.spoilerButton').hide();
-  $('.spoiler').slideDown(2000);  
+  $('.spoiler').slideDown(2000);
 });
+
 
 
